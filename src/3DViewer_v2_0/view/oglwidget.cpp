d73cceb3227d0672764ea66343dc41e0e42a011a
apjam <apjam@yandex.ru>
2023-12-10 17:53:37 +0600

    update paint

--- src/3DViewer_v2_0/view/oglwidget.cpp
+++ src/3DViewer_v2_0/view/oglwidget.cpp
@@ -3,8 +3,7 @@
 OGLWidget::OGLWidget(QWidget *parent) : QOpenGLWidget{parent} {}
 void OGLWidget::initializeGL() {
   initializeOpenGLFunctions();
-  //glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-//  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_DEPTH_TEST);
 }
 
 void OGLWidget::resizeGL(int w, int h) { glViewport(0, 0, w, h); }
@@ -14,32 +13,38 @@ void OGLWidget::paintGL() {
                backgroundColor.blueF(), 1);
   glClear(GL_COLOR_BUFFER_BIT);
 
-  // styles
-  glLineWidth(lineSize);
-  glPointSize(pointSize);
-  glColor3f(lineColor.redF(), lineColor.greenF(), lineColor.blueF());
-  if (lineType) {
-    glEnable(GL_LINE_STIPPLE);
-    glLineStipple(1, 0x0101);
-  } else {
-    glDisable(GL_LINE_STIPPLE);
-  }
-  if (pointType == 1) {
-    glEnable(GL_POINT_SMOOTH);
-  } else if (pointType == 2) {
-    glDisable(GL_POINT_SMOOTH);
-  }
-
   if (data_ != nullptr) {
-    glVertexPointer(3, GL_DOUBLE, 0, data_->Vertices());
     glEnableClientState(GL_VERTEX_ARRAY);
-    glEnable(GL_POINT_SMOOTH);
-    glDisable(GL_POINT_SMOOTH);
-    glDrawArrays(GL_POINTS, 0, data_->VertexCount());
+    glVertexPointer(3, GL_DOUBLE, 0, data_->Vertices());
 
-    //glColor3d(pointColor.redF(), pointColor.greenF(), pointColor.blueF());
-    glLineStipple(1, 0x00FF);
+//    if (perspective)
+//      glFrustum(-1, 1, -1.0, 1.0, 1.0, 100.0);
+//    else
+//      glOrtho(-2, 2, -1, 1, -10, 10);
+
+    if (lineType) {
+      glEnable(GL_LINE_STIPPLE);
+      glLineStipple(1, 0x0101);
+    } else {
+      glDisable(GL_LINE_STIPPLE);
+    }
+    glLineWidth(lineSize);
+    glColor3f(lineColor.redF(), lineColor.greenF(), lineColor.blueF());
+
     glDrawElements(GL_LINES, data_->FaceIndCount(), GL_UNSIGNED_INT, data_->FaceIndices());
+
+    if (pointType != 0) {
+      if (pointType == 1) {
+        glEnable(GL_POINT_SMOOTH);
+      } else if (pointType == 2) {
+        glDisable(GL_POINT_SMOOTH);
+      }
+      glPointSize(pointSize);
+      glColor3d(pointColor.redF(), pointColor.greenF(), pointColor.blueF());
+
+      glDrawArrays(GL_POINTS, 0, data_->VertexCount());
+    }
+
     glDisableClientState(GL_VERTEX_ARRAY);
   }
 }
