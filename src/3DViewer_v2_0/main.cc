23e8213f7131b1adc354c777c4384b6bccd2c2d4
Sandor Merle <sandorme@ob-e9.nsk.21-school.ru>
2023-12-19 18:58:44 +0700

    locale

--- src/3DViewer_v2_0/main.cc
+++ src/3DViewer_v2_0/main.cc
@@ -3,7 +3,7 @@
 #include "view/mainwindow.h"
 
 int main(int argc, char *argv[]) {
-  setlocale(LC_NUMERIC, "C");
+  QLocale::setDefault(QLocale::C);
   QApplication a(argc, argv);
   s21::MainWindow w;
   w.show();
